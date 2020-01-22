fec6f0e81dce7331db6f8886504a727931c165e7
runacode <59107019+runacode@users.noreply.github.com>
2020-01-21 20:43:54 -0600

    Auto move

--- thankyou.php
+++ thankyou.php
@@ -200,7 +200,24 @@ if ($orderItem) {
 }
 
 ?>
+<?php if(isset($data->Lo_Site_Id)) {
+    ?>
+    }
+    <script type='text/javascript'>
+        window.__lo_site_id = <?php echo $data->Lo_Site_Id; ?>;
 
+            (function () {
+                var wa = document.createElement('script');
+                wa.type = 'text/javascript';
+                wa.async = true;
+                wa.src = 'https://d10lpsik1i8c69.cloudfront.net/w.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(wa, s);
+            })();
+    </script>
+    <?php
+}
+?>
 <script>
     $.removeCookie('cart', {path: '/'});
 
